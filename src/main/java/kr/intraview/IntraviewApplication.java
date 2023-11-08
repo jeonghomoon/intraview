e4642e87c0b350e1bc58c1eae537699a9b9b2b1e
jeonghomoon <jeonghomoon@yahoo.com>
2023-11-08 21:48:49 +0900

    chore: initial commit

---
+++ src/main/java/kr/intraview/IntraviewApplication.java
@@ -0,0 +1,13 @@
+package kr.intraview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntraviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntraviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/intraview/IntraviewApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.intraview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntraviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
