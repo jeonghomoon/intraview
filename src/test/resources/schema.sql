d58ae51152494df2cda9f5e48104ee875aaf0106
jeonghomoon <jeonghomoon@yahoo.com>
2023-11-10 21:22:38 +0900

    test: change h2 mode to mysql for testing

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS "user" (
+CREATE TABLE IF NOT EXISTS `user` (
   id CHAR(36),
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   PRIMARY KEY (id),
-  UNIQUE (email)
+  UNIQUE KEY (email)
 );
