257d58847733e6700feb691adf0c8290b07a2f98
jeonghomoon <jeonghomoon@yahoo.com>
2023-11-13 23:46:59 +0900

    feat: encode user password when register

--- src/main/java/kr/intraview/service/UserServiceImpl.java
+++ src/main/java/kr/intraview/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package kr.intraview.service;
 
 import java.util.UUID;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.intraview.mapper.UserMapper;
@@ -11,17 +12,23 @@ import kr.intraview.model.UserDTO;
 @Service
 public class UserServiceImpl implements UserService {
 
+  private final PasswordEncoder passwordEncoder;
   private final UserMapper userMapper;
 
-  public UserServiceImpl(UserMapper userMapper) {
+  public UserServiceImpl(
+    PasswordEncoder passwordEncoder,
+    UserMapper userMapper
+  ) {
+    this.passwordEncoder = passwordEncoder;
     this.userMapper = userMapper;
   }
 
   public void createUser(UserDTO userDto) {
+    String encodedPassword = passwordEncoder.encode(userDto.getPassword());
     User user = new User(
       UUID.randomUUID().toString(),
       userDto.getEmail(),
-      userDto.getPassword()
+      encodedPassword
     );
     userMapper.insertUser(user);
   }
--- src/test/java/kr/intraview/service/UserServiceImplTest.java
+++ src/test/java/kr/intraview/service/UserServiceImplTest.java
@@ -9,12 +9,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import kr.intraview.mapper.UserMapper;
 import kr.intraview.model.UserDTO;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
+
+  @Mock
+  PasswordEncoder passwordEncoder;
 
   @Mock
   UserMapper userMapper;
