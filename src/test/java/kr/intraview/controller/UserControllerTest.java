56428d5c8bb441c723365de8087160bccaa21f88
jeonghomoon <jeonghomoon@yahoo.com>
2023-11-13 23:43:19 +0900

    feat: add spring security dependencies

--- src/test/java/kr/intraview/controller/UserControllerTest.java
+++ src/test/java/kr/intraview/controller/UserControllerTest.java
@@ -1,5 +1,6 @@
 package kr.intraview.controller;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
@@ -7,11 +8,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
+import kr.intraview.SecurityConfig;
 import kr.intraview.service.UserService;
 
 @WebMvcTest(UserController.class)
+@Import(SecurityConfig.class)
 public class UserControllerTest {
 
   @Autowired
@@ -28,6 +32,7 @@ public class UserControllerTest {
 
     // when & then
     mockMvc.perform(post("/users/register")
+      .with(csrf())
       .param("email", email)
       .param("password", password))
       .andExpect(redirectedUrl("/login"));
